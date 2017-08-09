ec1a63b95b62019ad2fce639da1ea0afd5afde93
Thomas Brüggemann <mail@thomasbrueggemann.com>
2017-08-09 13:35:35 +0200

    no message

--- sync.sh
+++ sync.sh
@@ -5,7 +5,8 @@ set -e
 echo "Job started: $(date)"
 
 cp -R "$DATA_PATH" /tmp/data
-/usr/local/bin/s3cmd sync $PARAMS /tmp/data "$S3_PATH"
+rm -R /tmp/data/.git
+/usr/local/bin/s3cmd sync $PARAMS /tmp/data/ "$S3_PATH"
 rm -R /tmp/data
 
 echo "Job finished: $(date)"
